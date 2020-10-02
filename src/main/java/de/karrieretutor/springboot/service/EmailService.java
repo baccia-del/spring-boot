9323053e5ffac734e62cf2009e4a881e70ac6c80
artingo <coach@artingo.net>
2020-10-02 11:04:16 +0200

    Removed java email template

--- src/main/java/de/karrieretutor/springboot/service/EmailService.java
+++ src/main/java/de/karrieretutor/springboot/service/EmailService.java
@@ -1,6 +1,5 @@
 package de.karrieretutor.springboot.service;
 
-import de.karrieretutor.springboot.domain.BestelltesProdukt;
 import de.karrieretutor.springboot.domain.Bestellung;
 import de.karrieretutor.springboot.domain.Kunde;
 import org.slf4j.Logger;
@@ -61,48 +60,6 @@ public class EmailService {
 
         try {
             sendMessageWithAttachment(empfaenger, betreff, htmlContent);
-        } catch (MessagingException e) {
-            LOG.error(e.getMessage(), e.getStackTrace());
-        }
-    }
-
-    public void versandBestaetigung(Bestellung bestellung) {
-        Kunde kunde = bestellung.getKunde();
-        String empfaenger = kunde.getEmail();
-        String betreff = "[Demo-Shop] Versandbestätigung für Ihre Bestellung Nr. " + bestellung.getId()
-                + " von " + bestellung.getGesamtzahl() + " Artikeln";
-
-        StringBuilder b = new StringBuilder(1000);
-        b.append("<p>Hallo ").append(kunde.getNameFormatiert()).append(",<br/><br/>");
-        b.append("Ihre Bestellung Nr.").append(bestellung.getId()).append(" wurde versendet.</p>");
-
-        b.append("<p>Die Lieferung enthält folgende Artikel:<br/>");
-        for (BestelltesProdukt bp : bestellung.getProdukte()) {
-            b.append("Bezeichnung: ").append(bp.getProdukt().getName())
-                    .append(", Preis: ").append(bp.getProdukt().getPreisFormatiert())
-                    .append(", Anzahl: ").append(bp.getAnzahl() + "<br/>");
-        }
-        b.append("Gesamtzahl bestellter Produkte: ").append(bestellung.getGesamtzahl())
-                .append(", Gesamtpreis: ").append(bestellung.getGesamtpreis() + "<br/>");
-        b.append("</p>");
-
-        b.append("<p>Es kann keine Änderung mehr an Ihrer Bestellung vorgenommen werden, da sie sich bereits im Versand befindet</p>");
-
-        b.append("<p>Ihre Bestellung sollte in den nächsten 3 Werktage bei Ihnen ankommen.<br/>");
-        b.append("Versendet wurde an folgende Adresse:<br/>");
-        b.append(kunde.getNameFormatiert() + "<br/>");
-        b.append(kunde.getStrasse() + "<br/>");
-        b.append(kunde.getPlz() + " ").append(kunde.getOrt() + "</p>");
-
-        b.append("<p>Zahlungsart:<br/>");
-        b.append(kunde.getZahlungsart());
-        b.append("</p>");
-
-        b.append("Mit freundlichen Grüßen,<br/>");
-        b.append("Ihr Demo-Shop-Team");
-
-        try {
-            sendMessageWithAttachment(empfaenger, betreff, b.toString());
         } catch (MessagingException e) {
             LOG.error(e.getMessage(), e.getStackTrace());
         }
